c174515f72100f311183310dedb8d0b944d0a099
Shuo Qi <MagicianQi@users.noreply.github.com>
2019-07-15 15:44:26 +0800

    Add files via upload

---
+++ evaluate_weights.py
@@ -0,0 +1,33 @@
+
+t_file = open("./data/news.test", "r")
+w_file = open("./output/test_results.tsv", "r")
+o_file = open("./output/weights_result.txt", "w")
+
+seq_length = 128
+text_list = []
+
+for line in t_file.readlines():
+    s_data = line.strip().split("\t")
+    text = s_data[1].strip().replace(" ", "").replace("\t", "").replace("　", "")
+    temp = []
+    temp.append(["[CLS]"])
+    for char in text[0:(seq_length-2)]:
+        temp.append([char])
+    temp.append(["[SEP]"])
+    text_list.append(temp)
+
+n = 0
+
+for line in w_file.readlines():
+    s_data = line.strip().split("|")
+    weights = s_data[-1].strip().split(",")
+    for each, w in zip(text_list[n], weights):
+        each.append(float(w))
+    n += 1
+
+for item in text_list:
+    item = sorted(item, key=lambda x: x[1], reverse=True)
+    for char in item:
+        o_file.write("{}\t{}\n".format(char[0], char[1]))
+    o_file.write("--------------------------\n")
+
